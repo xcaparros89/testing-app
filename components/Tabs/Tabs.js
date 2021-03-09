8713f0b932283c15047a45c607395d74825f1edd
Xavier Caparros <xcaparros@hotmail.com>
2021-03-09 13:04:59 +0100

    added all tests

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,30 +1,37 @@
 import { useState, useEffect } from "react";
 import styles from "./Tabs.module.css";
-import {slugify} from '../../utils/slugify'
-import { useRouter } from 'next/router'
+import { slugify } from "../../utils/slugify";
+import PropTypes from "prop-types";
+import { useRouter } from "next/router";
 
 export const Tabs = ({ children, initialTab }) => {
-  const [activeTab, setActiveTab] = useState(children[0].props.label);
+  const [activeTab, setActiveTab] = useState(slugify(children[0].props.label));
   const router = useRouter();
   const handleClick = (e, newActiveTab) => {
     e.preventDefault();
     setActiveTab(slugify(newActiveTab));
   };
-  useEffect(()=>{
-      if(initialTab.tab){
-          setActiveTab(initialTab.tab);
-      }
-  },[])
-  useEffect(()=>{
-      router.push(`${router.pathname}?tab=${slugify(activeTab)}`, undefined, {shallow:true})
-  }, [activeTab])
+  useEffect(() => {
+    if (initialTab.tab) {
+      setActiveTab(initialTab.tab);
+    }
+  }, []);
+  useEffect(() => {
+    router.push(`${router.pathname}?tab=${slugify(activeTab)}`, undefined, {
+      shallow: true,
+    });
+  }, [activeTab]);
   return (
     <div>
       <ul className={styles.tabs}>
         {children.map((tab, i) => {
           const label = tab.props.label;
           return (
-            <li className={slugify(label) === activeTab ? styles.current : ""} key={i}>
+            <li
+              data-testid={slugify(label)}
+              className={slugify(label) === activeTab ? styles.current : ""}
+              key={i}
+            >
               <a href="#" onClick={(e) => handleClick(e, label)}>
                 {label}
               </a>
@@ -35,7 +42,7 @@ export const Tabs = ({ children, initialTab }) => {
       {children.map((child, i) => {
         if (slugify(child.props.label) === activeTab) {
           return (
-            <div key={i} className={styles.content}>
+            <div data-testid="content" key={i} className={styles.content}>
               {child.props.children}
             </div>
           );
@@ -43,4 +50,17 @@ export const Tabs = ({ children, initialTab }) => {
       })}
     </div>
   );
+};
+
+Tabs.propTypes = {
+  initialTab: PropTypes.object,
+  children: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      children: PropTypes.any,
+    })
+  ).isRequired,
+};
+Tabs.defaultProps = {
+  initialTab: {},
 };
--- components/Tabs/Tabs.test.js
+++ components/Tabs/Tabs.test.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom'
 import { Tabs } from "./Tabs";
 import { useRouter } from "next/router";
 
 jest.mock("next/router");
+beforeEach(()=>{
+    useRouter.mockReturnValue({
+        push: () => {},
+      });
+})
 test("<Tabs /> matches snapshot", () => {
-  useRouter.mockReturnValue({
-    push: () => {},
-  });
   const component = render(
     <Tabs initialTab={{ tab: "tab-3" }}>
       <div label="Tab 1">
@@ -26,4 +29,92 @@ test("<Tabs /> matches snapshot", () => {
     </Tabs>
   );
   expect(component.container).toMatchSnapshot();
+});
+
+test("<Tabs /> renders without crashing", () => {
+  const div = document.createElement("div");
+  render(
+    <Tabs initialTab={{ tab: "tab-3" }}>
+      <div label="Tab 1">
+        <h2>Title 1</h2>
+        <p>Text 1</p>
+      </div>
+      <div label="Tab 2">
+        <h2>Title 2</h2>
+        <p>Text 2</p>
+      </div>
+      <div label="Tab 3">
+        <h2>Title 3</h2>
+        <p>Text 3</p>
+      </div>
+    </Tabs>,
+    div
+  );
+});
+test("Defaults to the first tab's content", () => {
+  const div = document.createElement("div");
+  const {getByTestId} = render(
+    <Tabs>
+      <div label="Tab 1">
+        1
+      </div>
+      <div label="Tab 2">
+        2
+      </div>
+      <div label="Tab 3">
+        3
+      </div>
+    </Tabs>,
+    div
+  );
+  const content = getByTestId("content");
+  expect(content.textContent).toBe('1')
+});
+test("Can set a different tab as the initial state", () => {
+    const div = document.createElement("div");
+    const {getByTestId} = render(
+      <Tabs initialTab={{ tab: "tab-3" }}>
+        <div label="Tab 1">
+          1
+        </div>
+        <div label="Tab 2">
+          2
+        </div>
+        <div label="Tab 3">
+          3
+        </div>
+      </Tabs>,
+      div
+    );
+    const content = getByTestId("content");
+    expect(content.textContent).toBe('3')
+});
+test("Click from one tab to the next", () => {
+    const div = document.createElement("div");
+    const {getByTestId, getByText} = render(
+      <Tabs initialTab={{ tab: "tab-3" }}>
+        <div label="Tab 1">
+          1
+        </div>
+        <div label="Tab 4">
+          4
+        </div>
+        <div label="Tab 3">
+          3
+        </div>
+      </Tabs>,
+      div
+    );
+    let content = getByTestId("content");
+    expect(content.textContent).toBe('3')
+    //differentiate the current tab
+    const li = getByTestId('tab-3');
+    expect(li).toHaveClass('current');
+    //click on a new tab
+    const differentTab = getByText('Tab 4');
+    fireEvent.click(differentTab);
+    const diffLi = getByTestId('tab-4');
+    expect(diffLi).toHaveClass('current');
+    content = getByTestId("content");
+    expect(content.textContent).toBe('4')
 });
